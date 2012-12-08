42cb974f21395982250beecccc98964110aad367
Krzysztof Krzysztofik <krzysztof.krzysztofik@teonite.com>
2012-12-08 23:59:41 +0100

    #1081 - mysql part pre-alpha

--- deploy.py
+++ deploy.py
@@ -4,6 +4,7 @@ import os
 import sys
 from os.path import dirname, abspath
 import re
+from datetime import datetime
 
 sys.path.append(dirname(dirname(abspath(__file__))))
 
@@ -119,22 +120,64 @@ def	src_remote_rollback(dir, host, user):
 	_pretty_print("Remote rollback finished")
 
 def deploy():
+	_pretty_print("Starting deployment.")
 	src_clone()
 	src_prepare()
 	src_remote_extract()
 	src_remote_deploy()
+	_pretty_print("Deployment finished.")
 
-def	mysql_db_dump():
-	pass
+def	mysql_db_dump(filename, database, dbuser, dbpassword, host, host_user):
+	_pretty_print('Starting MySQL dump.')
+	env.hosts = [host]
+	env.user = host_user
+	env.use_ssh_config = True
 
-def	mysql_db_restore():
-	pass
+	run('mysqldump -u%s -p%s %s > %s' %(dbuser, dbpassword, database, filename))
+	_pretty_print('MySQL dump finished.')
 
-def	mysql_db_clone():
-	pass
+def	mysql_db_restore(filename, database, dbuser, dbpassword, host, host_user):
+	_pretty_print('Starting MySQL restore.')
+	env.hosts = [host]
+	env.user = host_user
+	env.use_ssh_config = True
 
-def	mysql_db_migrate():
-	pass
+	run('mysql -u%s -p%s %s < %s' % (dbuser, dbpassword, database, filename))
+	_pretty_print('MySQL restore finished.')
+
+def	mysql_db_clone(database, dbuser, dbpassword, host, host_user):
+	_pretty_print('Starting MySQL clone.')
+
+	env.hosts = [host]
+	env.user = host_user
+	env.use_ssh_config = True
+
+	new_database = '%s-%s' % (database, datetime.now().strftime("%Y%m%d"))
+
+	mysql_db_dump('temp.sql', database, dbuser, dbpassword, host, host_user)
+	run('mysql -u%s -p%s %s < %s' % (dbuser, dbpassword, database,
+									 'CREATE DATABASE %s' % new_database))
+	mysql_db_restore('temp.sql', new_database, dbuser, dbpassword, host, host_user)
+
+	_pretty_print('MySQL clone finished.')
+
+def list_dir(dir_=None):
+	"""returns a list of files in a directory (dir_) as absolute paths"""
+	dir_ = dir_ or env.cwd
+	string_ = run("for i in %s*; do echo $i; done" % dir_)
+	files = string_.replace("\r","").split("\n")
+	return files
+
+def	mysql_db_migrate(database, dir, dbuser, dbpassword, host, host_user):
+	_pretty_print('Starting MySQL migrate')
+	env.hosts = [host]
+	env.user = host_user
+	env.use_ssh_config = True
+	with(cd(dir)):
+		for file in list_dir():
+			run('mysql -u%s -p%s %s < %s' % (dbuser, dbpassword, database, file))
+
+	_pretty_print('MySQL migrate finished.')
 
 def	db_migrate():
 	mysql_db_dump()
