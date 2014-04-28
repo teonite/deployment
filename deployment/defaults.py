51ddb4dac3b2d8acb4f3a9f97922d11cb8360cff
Krzysztof Krzysztofik <krzysztof.krzysztofik@teonite.com>
2014-04-28 16:36:19 +0200

    [RM #3210] log level info as default

--- deployment/defaults.py
+++ deployment/defaults.py
@@ -29,7 +29,8 @@ config = {
     "venv": {
         "dir": "env",
         "requirements": ["requirements/production.txt"],
-        "update": False
+        "update": False,
+        "check": False,
     },
 
     "config": {
@@ -78,3 +79,45 @@ path_list = [
         './conf/deployment/production.json',
         './settings/deployment/production.json'
 ]
+
+LOGGING = {
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "%(message)s"
+        },
+        "verbose": {
+            "format": "[%(asctime)s] \"%(message)s\"",
+            "datefmt": "%d/%b/%Y %H:%M:%S"
+        }
+    },
+
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout"
+        },
+        "graypy": {
+            "level": "INFO",
+            "class": "graypy.GELFHandler",
+            "formatter": "verbose",
+            "host": "logs.teonite.net",
+            "port": 12201
+        }
+    },
+
+    "loggers": {
+        "root": {
+            "handlers": ["console"],
+            "level": "INFO"
+        },
+        "deployment": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "qualname": "deployment",
+            "propagate": False
+        }
+    }
+}
